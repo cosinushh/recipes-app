eff16ff15f806ce61bde2af3201ec1d1d3ccb66d
Kai Franke <cosinushh@gmail.com>
2022-10-04 16:50:02 +0200

    add Image to Dessert

--- pages/dessert.js
+++ pages/dessert.js
@@ -1,7 +1,15 @@
+import Image from "next/image";
 export default function Dessert() {
   return (
     <>
       <h1>Hier gibts Dessert</h1>
+      <Image
+        src="https://unsplash.com/photos/surQ2mkZNxw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mzd8fGRlc3NlcnR8ZW58MHwwfHx8MTY1OTMzNzk1NQ&force=true&w=2400"
+        width={2400}
+        height={1599}
+        alt="kuchen"
+        layout="responsive"
+      />
       <p>
         .tema tis rolod muspi meroL tse sutcnas atamikat aes on ,nergrebug dsak
         atilc tetS .muber ae te serolod oud otsuj te masucca te soe orev tA
